79aecae9829c3a6b3350c497b31e5fdd935ae913
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-29 21:36:06 +0530

    Optimized plotting using animation

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import sys
 
+
+fig, graph = plt.subplots()
 
 def plot(arr):
     plt.clf()
@@ -12,12 +15,16 @@ def on_close(event):
     if not sys.argv[0] == 'test.py':
         sys.exit(0)
 
+def call(arr):
+    plt.bar(range(len(arr)), arr, align='edge', width=1, data=arr)
 
 def show():
+    #My approach to speed up the plotting process would be to use matplotlib's animation. Following is a partially wrong implementation. Uncomment it to run...
+    #myAnimation = animation.FuncAnimation(fig, call, interval=5, blit=True, repeat=True)
     plt.show()
 
 
-fig, graph = plt.subplots()
+
 fig.canvas.mpl_connect('close_event', on_close)
 graph.yaxis.set_visible(False)
 graph.xaxis.set_visible(False)
